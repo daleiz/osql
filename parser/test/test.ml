cf6d26b42935fb216d35bcdb32e49ed87c8700c7
Wangbin <wangbin@emqx.io>
2020-10-13 17:10:50 +0800

    Rename LibraryName__Module to LibraryName.Module

--- parser/test/test.ml
+++ parser/test/test.ml
@@ -1,19 +1,19 @@
 let test_parse_create_stmt () =
   let expect = "create stream s (id int, name string, flag bool)" in
-  let result = Parser__Main.parse_str expect in
-  let actual = Parser__Sql.stmt_to_str result in 
+  let result = Parser.Main.parse_str expect in
+  let actual = Parser.Sql.stmt_to_str result in 
   Alcotest.(check string) "same string" expect actual
 
 let test_parse_insert_stmt () =
   let expect = {|insert into s (id, name, flag) values ((0, "neo", true), (1, "mike", false))|} in
-  let result = Parser__Main.parse_str expect in
-  let actual = Parser__Sql.stmt_to_str result in 
+  let result = Parser.Main.parse_str expect in
+  let actual = Parser.Sql.stmt_to_str result in 
   Alcotest.(check string) "same string" expect actual
 
 let test_parse_select_stmt () =
   let expect = "select id, name, flag from s where id > 0 group by id" in
-  let result = Parser__Main.parse_str expect in
-  let actual = Parser__Sql.stmt_to_str result in 
+  let result = Parser.Main.parse_str expect in
+  let actual = Parser.Sql.stmt_to_str result in 
   Alcotest.(check string) "same string" expect actual
 
 let tests = [
--- test/run_tests.ml
+++ test/run_tests.ml
@@ -3,7 +3,7 @@
 *)
 
 let test_suites: unit Alcotest.test list = [
-  "osql_parser_test", Test_parser__Test.tests;
+  "osql_parser_test", Test_parser.Test.tests;
 ]
 
 let () = Alcotest.run "osql" test_suites
